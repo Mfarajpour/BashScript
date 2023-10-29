6cec175572359c0a0487682c553187a2a1de0ca7
mfarajpour <mohsenfarajpour1366@gmail.com>
2023-10-29 14:31:39 +0000

    arithmetic operators

---
+++ example/10-1_arithmetic_operators.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+a=10
+b=3
+
+sum=`expr $a + $b`
+echo $sum
+
+sub=`expr $a - $b`
+echo $sub
+
+mul=`expr $a \* $b`
+echo $mul
+
+
+div=`expr $a / $b`
+echo $div
+
+
+
---
+++ example/10_arithmetic_operators.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+a=8
+b=3
+c=2
+
+
+echo " a = 8 "
+echo " b = 3 "
+echo " c = 2 "
+echo "-------"
+echo " a + b = " $(($a + $b))
+echo " a - b = " $(($a - $b))
+echo " a * b = " $(($a * $b))
+echo " a / c = " $(($a / $c))
+echo " a % b = " $(($a % $b))
